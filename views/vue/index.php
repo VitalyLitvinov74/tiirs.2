0c7cef5dd224eff19493d60d43d56c45049e238e
Vitaly Litvinov <cheltend201@yandex.ru>
2021-10-15 17:01:49 +0500

    Создал перенаправление на vue.js
    далее необходимо сгенерировать app.js от вью через вебпак

---
+++ views/vue/index.php
@@ -0,0 +1,8 @@
+<div class="container" id="app">
+    <transition>
+        <router-view></router-view>
+    </transition>
+    <!--        <div v-if="this.$route.matched.length == 0">-->
+    <!--            --><?//= $content ?>
+    <!--        </div>-->
+</div>
